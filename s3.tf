fef1726723dd0b7f003a3adc0d1956511f3424a2
chiranjeevi keerthi <chiranjeevikeerthi77777@gmail.com>
2025-04-01 14:00:09 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 # Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swiggy-chirubucket-terraform454"
+  bucket = "chiru_hcpbucket254"
 
   # Enable versioning
   versioning {
