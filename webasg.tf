b792b24f796a46f44cdbe750ddbf1499fd70d631
chiranjeevi keerthi <chiranjeevikeerthi77777@gmail.com>
2025-04-01 13:58:33 +0530

    Update webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -14,7 +14,7 @@ resource "aws_autoscaling_group" "swiggy-web-asg" {
 ###### Create a Launch Template for the EC2 instances ######
 resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
-  image_id      = "ami-01bd9d8f06d29d6a0"
+  image_id      = "ami-076c6dbba59aa92e6"
   instance_type = "t2.micro"
   key_name      = "devopsbyraham"
   network_interfaces {
