acf34196d1d3dda28fe43158d0b7158492aa6ffe
chiranjeevi keerthi <chiranjeevikeerthi77777@gmail.com>
2025-04-01 14:04:05 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,10 @@
-# Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "chiru_hcpbucket254"
-
-  # Enable versioning
-  versioning {
-    enabled = true
-  }
+  bucket = "swiggy-buckefuvghvhgc112565"
 }
 
+resource "aws_s3_bucket_versioning" "example_versioning" {
+  bucket = aws_s3_bucket.example_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
